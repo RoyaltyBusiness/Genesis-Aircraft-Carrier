0deed3c9ec6eca1ca25cc26611b7743a1a021da7
Royalty <royaltybusiness1999@gmail.com>
2024-10-18 23:04:38 +0200

    Add files via upload

---
+++ Genesis.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using VehicleFramework;
+using VehicleFramework.VehicleParts;
+using VehicleFramework.VehicleTypes;
+using System.IO;
+using System.Reflection;
+using UnityEngine.U2D;
+using System.Collections;
+using UWE;
+using static Nautilus.Assets.PrefabTemplates.FabricatorTemplate;
+using UnityEngine.Assertions;
+using VehicleFramework.Assets;
+using VehicleFramework.Engines;
+
+namespace GenesisCarrier
+{
+    public class Genesis : Submarine
+    {
+        public static IEnumerator Register()
+        {
+            GetAssets();
+            Submarine Genesis = assets.model.EnsureComponent<Genesis>();
+            Genesis.name = "Genesis";
+            yield return CoroutineHost.StartCoroutine(VehicleRegistrar.RegisterVehicle(Genesis));
+            yield break;
+        }
+        public static VehicleFramework.Assets.VehicleAssets assets;
+        public static void GetAssets()
+        {
+            assets = AssetBundleInterface.GetVehicleAssetsFromBundle("assets/genesis", "Genesis_vehicle");
+        }
+        public override List<VehiclePilotSeat> PilotSeats
+        {
+            get
+            {
+                var list = new List<VehicleFramework.VehicleParts.VehiclePilotSeat>();
+                VehicleFramework.VehicleParts.VehiclePilotSeat vps = new VehicleFramework.VehicleParts.VehiclePilotSeat();
+                Transform mainSeat = transform.Find("PilotSeat");
+                vps.Seat = mainSeat.gameObject;
+                vps.SitLocation = mainSeat.Find("SitLocation").gameObject;
+                vps.LeftHandLocation = mainSeat;
+                vps.RightHandLocation = mainSeat;
+                vps.ExitLocation = mainSeat.Find("ExitLocation");
+                list.Add(vps);
+                return list;
+            }
+        }
+
+        public override List<VehicleHatchStruct> Hatches
+        {
+            get
+            {
+                var list = new List<VehicleFramework.VehicleParts.VehicleHatchStruct>();
+
+                VehicleFramework.VehicleParts.VehicleHatchStruct interior_vhs = new VehicleFramework.VehicleParts.VehicleHatchStruct();
+                Transform intHatch = transform.Find("Hatches/InsideHatch");
+                interior_vhs.Hatch = intHatch.gameObject;
+                interior_vhs.EntryLocation = intHatch.Find("Entry");
+                interior_vhs.ExitLocation = intHatch.Find("Exit");
+                interior_vhs.SurfaceExitLocation = intHatch.Find("SurfaceExit");
+
+                VehicleFramework.VehicleParts.VehicleHatchStruct exterior_vhs = new VehicleFramework.VehicleParts.VehicleHatchStruct();
+                Transform extHatch = transform.Find("Hatches/OutsideHatch");
+                exterior_vhs.Hatch = extHatch.gameObject;
+                exterior_vhs.EntryLocation = interior_vhs.EntryLocation;
+                exterior_vhs.ExitLocation = interior_vhs.ExitLocation;
+                exterior_vhs.SurfaceExitLocation = interior_vhs.SurfaceExitLocation;
+
+                list.Add(interior_vhs);
+                list.Add(exterior_vhs);
+                return list;
+            }
+        }
+
+        public override GameObject VehicleModel
+        {
+            get
+            {
+                return assets.model;
+            }
+        }
+
+        public override GameObject CollisionModel
+        {
+            get
+            {
+                return transform.Find("Collider").gameObject;
+            }
+        }
+        public override List<GameObject> TetherSources
+        {
+            get
+            {
+                var list = new List<GameObject>();
+                foreach (Transform child in transform.Find("TetherSources"))
+                {
+                    list.Add(child.gameObject);
+                }
+                return list;
+            }
+        }
+        public override GameObject BoundingBox
+        {
+            get
+            {
+                return transform.Find("BoundingBoxCollider").gameObject;
+            }
+        }
+        public override List<VehicleUpgrades> Upgrades
+        {
+            get
+            {
+                var list = new List<VehicleFramework.VehicleParts.VehicleUpgrades>();
+                VehicleFramework.VehicleParts.VehicleUpgrades vu = new VehicleFramework.VehicleParts.VehicleUpgrades();
+                vu.Interface = transform.Find("Upgrades").gameObject;
+                vu.Flap = vu.Interface;
+                list.Add(vu);
+                return list;
+            }
+        }
+        public override ModVehicleEngine Engine
+        {
+            get
+            {
+                return gameObject.EnsureComponent<ACarrierEngine>();
+
+            }
+
+        }
+        public override int Mass => 5000;
+        public override int NumModules => 2;
+    }
+}
---
+++ MainPatcher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleFramework.VehicleTypes;
+using System.Collections;
+using VehicleFramework;
+using GenesisCarrier;
+using BepInEx;
+using HarmonyLib;
+using Nautilus.Utility;
+
+namespace GenesisCarrier
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            UnityEngine.Debug.Log("[Genesis]:" + message);
+        }
+        public static void Output(string msg)
+        {
+            BasicText message = new BasicText(500, 0);
+            message.ShowMessage(msg, 5);
+        }
+    }
+    [BepInPlugin("com.royalty.subnautica.genesis.mod", "Genesis", "0.5.0")]
+    [BepInDependency("com.mikjaw.subnautica.vehicleframework.mod")]
+    [BepInDependency("com.snmodding.nautilus")]
+
+    public class MainPatcher : BaseUnityPlugin
+    {
+        public void Start()
+        {
+            var harmony = new Harmony("com.royalty.subnautica.genesis.mod");
+            harmony.PatchAll();
+            UWE.CoroutineHost.StartCoroutine(Genesis.Register());
+        }
+    }
+}
